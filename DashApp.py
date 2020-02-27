3cb7ea49800042b97a52f977f90459144e89f5b1
roytje88 <royvanrhedenburg@gmail.com>
2020-02-27 23:54:30 +0100

    added autorefresh to dash

--- DashApp.py
+++ DashApp.py
@@ -1,39 +1,143 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-exec(open("./trellopy.py").read())
+#exec(open("./creategraphdata.py").read())
+configurationfile = './configuration/configuration.txt'
+credentialsfile = './configuration/credentials.txt'
+import os,json
+def loadconfig():
+    global configfound
+    global credentialsfound
+    global config
+    global credentials
+    if os.path.exists(configurationfile):
+        with open(configurationfile) as json_file:
+            config = json.load(json_file)
+        configfound = True
+    else:
+        config = {}
+        for i,j in configoptions.items():
+            config[i] = j
+            configfound = False
+
+    if os.path.exists(credentialsfile):
+        with open(credentialsfile) as json_file:
+            credentials = json.load(json_file)
+        credentialsfound = True
+    else:
+        credentials = {}
+        for i,j in credentialsoptions.items():
+            credentials[i] = j
+        credentialsfound=False
+loadconfig()
+
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-layout = go.Layout(barmode='stack')
-
-fig = go.Figure(layout=layout, data=[go.Bar(name=chosenlists[0],x=x,y=Nieuw)])
-for i in chosenlists:
-    if i != chosenlists[0]:
-        if i not in config.get('Done'):
-            listname = i.replace(' ','_')
-            exec ("fig.add_trace(go.Bar(name=i,x=x,y=%s))" % (listname))
+import datetime
+import time
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 
-app.layout = html.Div(children=[
-    html.H1(children='Werkvoorraad Bedrijfsbureau'),
 
-    html.Div(children='''
-        Tijdlijn per lijst (niet Done)
-    '''),
 
-    dcc.Graph(figure=fig),
-    html.Div(children='''
-        Another timeline (just a copy for now)
-    '''),
-    dcc.Graph(figure=fig)
-])
+
+
+
+
+
+
+
+# number of seconds between re-calculating the data
+UPDADE_INTERVAL = 5
+
+def get_new_data():
+    """Updates the global variable 'data' with new data"""
+    global graphdata
+    global listfig
+    global statusfig
+
+    def loadjson(file):
+        with open('./data/'+file) as json_file:
+            return json.load(json_file)
+
+
+
+    chosenlists = loadjson('chosenlists.json')
+    graphdata = loadjson('graphdata.json')
+    statuses = loadjson('statuses.json')
+    statuslist = loadjson('statuslist.json')
+
+
+    layout = go.Layout(barmode='stack')
+
+    x = 0
+    for i in statuslist:
+        if x == 0:
+            if i != 'Archived':
+                statusfig = go.Figure(layout=layout, data=[go.Bar(name=i, x = graphdata.get('dates'), y = graphdata.get('Status '+i))])
+                x = 1
+                first = 'Status ' + i
+
+    for i,j in graphdata.items():
+        if i[:7] == 'Status ':
+            if i != first:
+                statusfig.add_trace(go.Bar(name=i[7:],x=graphdata.get('dates'),y=j))
+
+
+
+
+    listfig = go.Figure(layout=layout, data=[go.Bar(name=chosenlists[0],x = graphdata.get('dates'), y=graphdata.get(chosenlists[0]))])
+
+    for i in chosenlists:
+        if i != chosenlists[0]:
+            listfig.add_trace(go.Bar(name=i,x=graphdata.get('dates'),y=graphdata.get(i)))
+    return listfig
+    return statusfig
+
+
+
+
+
+def get_new_data_every(period=UPDADE_INTERVAL):
+    """Update the data every 'period' seconds"""
+
+    while True:
+        get_new_data()
+        time.sleep(period)
+
+
+def make_layout():
+    return html.Div(children=[
+        html.H1(children='Werkvoorraad'),
+        html.Div('Last update: ' + str(datetime.datetime.now())),
+        html.Div(children='''
+            Tijdlijn per lijst
+        '''),
+
+        dcc.Graph(figure=listfig),
+        html.Div(children='''
+            Tijdlijn per status
+        '''),
+        dcc.Graph(figure=statusfig)
+    ])
+
+app = dash.Dash(__name__)
+
+# get initial data
+get_new_data()
+
+# we need to set layout to be a function so that for each new page load
+# the layout is re-created with the current data, otherwise they will see
+# data that was generated when the Dash app was first initialised
+app.layout = make_layout
+
+# Run the function in another thread
+executor = ThreadPoolExecutor(max_workers=1)
+executor.submit(get_new_data_every)
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True,host='0.0.0.0')
+    app.run_server(debug=True)
